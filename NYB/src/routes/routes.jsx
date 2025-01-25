4e4b3204aea4ccdff8b63655e72395d588604c25
Nabiul Islam <154916844+Nabiul88@users.noreply.github.com>
2025-01-26 01:34:00 +0600

    Update routes.jsx

--- NYB/src/routes/routes.jsx
+++ NYB/src/routes/routes.jsx
@@ -6,7 +6,7 @@ import Order from "../pages/Order";
 import ScrollToTop from "../components/ScrollToTop";
 import ContactUs from "../pages/ContactUs";
 import Cart from "../pages/Cart";
-import Dashboard from "../layout/DashBoard";
+import Dashboard from "../layout/Dashboard";
 import ManageItems from "../Dashboard/ManageItems";
 import ManageOrder from "../Dashboard/ManageOrder";
 import Kitchen from "../Dashboard/Kitchen";
