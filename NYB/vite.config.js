7029918ea836f7b1741417394a05e44f0a84d09a
Nabiul Islam <154916844+Nabiul88@users.noreply.github.com>
2025-01-26 06:25:42 +0600

    Update vite.config.js

--- NYB/vite.config.js
+++ NYB/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes the server accessible on all network interfaces
+    port: 5173, // Ensure this matches your desired frontend port
+  },
 })
